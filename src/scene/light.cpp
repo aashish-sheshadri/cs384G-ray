c0d7a8de6ce8f682f63cd64708d1b13e7c43f21a
Aashish Sheshadri <aashish.sheshadri@gmail.com>
2013-10-12 00:55:43 -0500

    added distance attentuation

--- src/scene/light.cpp
+++ src/scene/light.cpp
@@ -9,8 +9,7 @@ using namespace std;
 double DirectionalLight::distanceAttenuation( const Vec3d& P ) const
 {
 	// distance to light is infinite, so f(di) goes to 0.  Return 1.
-	return 1.0;
-}
+	return 1;}
 
 
 Vec3d DirectionalLight::shadowAttenuation( const Vec3d& P ) const
@@ -41,9 +40,9 @@ double PointLight::distanceAttenuation( const Vec3d& P ) const
 	// You'll need to modify this method to attenuate the intensity 
 	// of the light based on the distance between the source and the 
 	// point P.  For now, we assume no attenuation and just return 1.0
-	return 1.0;
-
-}
+	Vec3d tempVec = P - position;
+	double d = tempVec.length();
+	return std::min(1.0,(double)1.0/(double)(constantTerm + linearTerm*d + quadraticTerm*d*d));}
 
 Vec3d PointLight::getColor( const Vec3d& P ) const
 {
