058ae7eb6ad55e57aae90082911e98bb84b0b754
Ivan Oropeza <oropivan@gto.ischool.utexas.edu>
2013-12-09 22:50:50 -0600

    added UV for sphere

--- src/SceneObjects/Sphere.cpp
+++ src/SceneObjects/Sphere.cpp
@@ -1,6 +1,7 @@
 #include <cmath>
 
 #include "Sphere.h"
+#define PI 3.14159265
 
 using namespace std;
 
@@ -24,17 +25,20 @@ bool Sphere::intersectLocal( const ray& r, isect& i ) const
 
 	i.obj = this;
 
-	double t1 = b - discriminant;
-
+    double t1 = b - discriminant;
 	if( t1 > RAY_EPSILON ) {
 		i.t = t1;
 		i.N = r.at( t1 );
-		i.N.normalize();
+        i.N.normalize();
 	} else {
 		i.t = t2;
 		i.N = r.at( t2 );
-		i.N.normalize();
+        i.N.normalize();
 	}
+
+    double uCor = 0.5f + atan2(-1*i.N[2],-1*i.N[0])/(2*PI),
+           vCor = 0.5f -  asin(-1*i.N[1])/PI;
+    i.setUVCoordinates(Vec2d(uCor,vCor));
 
 	return true;
 }
